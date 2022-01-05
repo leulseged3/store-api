374c37f476823d455aed91832377d8b4d404f352
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-05 11:01:32 +0300

    register user mongoose model

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './models/user.model';
 @Module({
-  providers: [UsersService]
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  providers: [UsersService],
+  controllers: [UsersController]
 })
-export class UsersModule {}
+export class UsersModule { }
