754204d31ec3a6413eb1c883a3402f9d32a519b8
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-05 18:08:24 +0300

    connect db with atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { SalesModule } from './sales/sales.module';
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    MongooseModule.forRoot(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`),
     UsersModule,
     AuthModule,
     SalesModule,
