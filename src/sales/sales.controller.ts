76f5106c1c20e25fea90d4a69be62a8fa8c0330c
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-05 15:42:53 +0300

    implement sales controller logic

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateSalesDto } from './dtos';
 import { SalesService } from './sales.service';
 
 @Controller('sales')
 export class SalesController {
-  constructor(private readonly salesService: SalesService) {}
+  constructor(private readonly salesService: SalesService) { }
+
+  @Post()
+  create(@Body() createSalesDto: CreateSalesDto) {
+    return this.salesService.create(createSalesDto)
+  }
+
+  @Get()
+  findAll() {
+    return this.salesService.findAll()
+  }
+
+  @Get('approved')
+  approved() {
+    return this.salesService.approvedSales()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.salesService.findOne(id)
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return this.salesService.update(id)
+  }
+
 }
