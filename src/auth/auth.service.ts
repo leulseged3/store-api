eb4a6e3adb29fc911c78b6e7152a90a30fbd6a6d
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-05 11:52:45 +0300

    implement validation and login logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthLoginDto } from './dtos/auth-login.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) { }
+
+  async login(authLoginDto: AuthLoginDto) {
+    const user = await this.validateUser(authLoginDto);
+
+    const payload = {
+      userId: user.id
+    }
+
+    return {
+      access_token: this.jwtService.sign(payload)
+    }
+  }
+
+  async validateUser(authLoginDto: AuthLoginDto) {
+    const { email, password } = authLoginDto;
+    const user = await this.userService.findByEmail(email);
+    const isPasswordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException()
+    }
+
+    return user
+  }
+}
