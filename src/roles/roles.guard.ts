e9a8d4b76e1d585ef256c2ebad4b4129bd25ce5c
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-05 14:02:47 +0300

    create roles guard

---
+++ src/roles/roles.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Role } from './role.enum';
+import { ROLES_KEY } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRole = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
+    if (!requiredRole) {
+      return true
+    }
+
+    const req = context.switchToHttp().getRequest();
+    return requiredRole.some(role => role === req.body.userType)
+  }
+}
