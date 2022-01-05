356eff29c5144a5a51fbeb562808be6438794733
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-05 10:56:50 +0300

    create users model

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -1,19 +1,13 @@
 import * as mongoose from 'mongoose';
 
 export const UserSchema = new mongoose.Schema({
-    fullName: {
-        firstName:{
-            type: String, required: true
-        },
-        lastName: {
-            type: String, required: true
-        }
-    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
-    password: {type: String, required: true},
-    userType: { 
+    password: { type: String, required: true },
+    userType: {
         type: String,
-        enum: ['USER','STORE_KEEPER','ADMIN'],
-        required : true
+        enum: ['USER', 'STORE_KEEPER', 'ADMIN'],
+        required: true
     }
-})
+});
